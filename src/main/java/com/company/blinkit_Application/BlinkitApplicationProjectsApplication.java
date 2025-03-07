316dbce251b4a9115b855e0681ad570b431c16a9
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-07 08:37:50 +0530

    Initialized the project for the blinkit-application

---
+++ src/main/java/com/company/blinkit_Application/BlinkitApplicationProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.company.blinkit_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkitApplicationProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkitApplicationProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/blinkit_Application/BlinkitApplicationProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.blinkit_Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkitApplicationProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
